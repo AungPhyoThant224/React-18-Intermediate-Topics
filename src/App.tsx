f7f1e291a860d81ad90c5ff8e669b11aa6b1f577
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-03 11:02:27 +0630

    Creating a Custom Provider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
-import LoginStatus from "./state-management/LoginStatus";
 import NavBar from "./state-management/NavBar";
-import tasksReducer from "./state-management/reducer/taskReducer";
 import TasksContext from "./state-management/context/tasksContext";
-import authReducer from "./state-management/reducer/authReducer";
-import AuthContext from "./state-management/context/authContext";
+import tasksReducer from "./state-management/reducer/taskReducer";
 
 function App() {
   const [tasks, taskDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
-      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
+      <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
         <NavBar />
         <HomePage />
-      </AuthContext.Provider>
-    </TasksContext.Provider>
+      </TasksContext.Provider>
+    </AuthProvider>
   );
 }
 
---
+++ src/state-management/AuthProvider.tsx
@@ -0,0 +1,18 @@
+import React, { Children, ReactNode, useReducer } from "react";
+import AuthContext from "./context/authContext";
+import authReducer from "./reducer/authReducer";
+
+interface Props {
+  children: ReactNode;
+}
+
+const AuthProvider = ({ children }: Props) => {
+  const [user, dispatch] = useReducer(authReducer, "");
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
