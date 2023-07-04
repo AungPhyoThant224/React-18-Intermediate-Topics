3f48b4bf9aab927a73b42ec06ad03cafaaec343b
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-04 09:48:33 +0630

    Getting Data About the Current Route.

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,16 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log(params);
+
+  const [searchParam, setSearchParam] = useSearchParams();
+  console.log(searchParam.toString());
+  console.log(searchParam.get("name"));
+
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
